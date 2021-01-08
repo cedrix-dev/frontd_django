f93023afcde8e491f6824d07a3bcabe5baae5e86
cedrix-dev <73937626+cedrix-dev@users.noreply.github.com>
2021-01-08 11:59:22 +0100

    Update urls.py

--- fdelectro/urls.py
+++ fdelectro/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[path('',views.index),path('register/',views.register1),path('signin/command/1',views.home),
-path('signin/command/2',views.command),path('signin/command/3',views.about),path('signin/command/5',views.register1),
+path('signin/command/2',views.command), path('signin/command/10',views.btn10),path('signin/command/11',views.btn11),path('signin/command/12',views.btn12),path('signin/command/13',views.btn13),path('signin/command/14',views.btn14),path('signin/command/15',views.btn15),path('signin/command/16',views.btn16),path('signin/command/3',views.about),path('signin/command/5',views.register1),
 path('signin/command/',views.signin1),path('fdelectro/aboutus',views.about),path('fdelectro/command',views.command),path('fdelectro/command/10',views.btn10),path('fdelectro/command/11',views.btn11),path('fdelectro/command/12',views.btn12),path('fdelectro/command/13',views.btn13),path('fdelectro/command/14',views.btn14),path('fdelectro/command/15',views.btn15),path('fdelectro/command/16',views.btn16),]
                                   
