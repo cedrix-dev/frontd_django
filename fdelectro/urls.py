81b1cf24c6fc922d9aa3c2d38eedd93f2056db56
cedrix-dev <73937626+cedrix-dev@users.noreply.github.com>
2021-01-08 10:48:36 +0100

    Update urls.py

--- fdelectro/urls.py
+++ fdelectro/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns=[path('',views.index),path('register/',views.register1),path('signin/command/1',views.home),
 path('signin/command/2',views.command),path('signin/command/3',views.about),path('signin/command/5',views.register1),
-path('signin/command/',views.signin1),path('fdelectro/aboutus',views.about),path('fdelectro/command',views.command),]
+path('signin/command/',views.signin1),path('fdelectro/aboutus',views.about),path('fdelectro/command',views.command),path('fdelectro/command/10',views.btn10),path('fdelectro/command/11',views.btn11),path('fdelectro/command/12',views.btn12),path('fdelectro/command/13',views.btn13),path('fdelectro/command/14',views.btn14),path('fdelectro/command/15',views.btn15),path('fdelectro/command/16',views.btn16),]
                                   
